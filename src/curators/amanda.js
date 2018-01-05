9fe6841a1858b30e95ffeee72c6d48a73b425838
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2018-01-05 16:59:20 -0500

    clean up albumList arrays

--- src/curators/amanda.js
+++ src/curators/amanda.js
@@ -4,10 +4,12 @@ const Amanda = {
     {
       albumTitle: "The Rooms of the House",
       artist: "La Dispute"
-    },{
+    },
+    {
       albumTitle: "Phoenix",
       artist: "Alazka"
-    },{
+    },
+    {
       albumTitle: "Congratulations",
       artist: "MGMT"
     }
