be946141ae329688ba706a310e98ae20defeeba4
Andrei Dobrinski <>
2018-02-04 21:29:50 -0500

    cleanup

--- src/components/labels/curator-label.js
+++ src/components/labels/curator-label.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Wrapper, Text } from './styled';
 
-const CuratorLabel = ({ curator }) => {
-  return (
-    <Wrapper>
-      <Text>
-        {curator.name}'s list
-      </Text>
-    </Wrapper>
-  );
-};
+const CuratorLabel = ({ curator }) => (
+  <Wrapper>
+    <Text>
+      {curator.name}'s list
+    </Text>
+  </Wrapper>
+);
 
 export default CuratorLabel;
--- src/components/labels/labels.js
+++ src/components/labels/labels.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { LabelsWrapper, Text, BolderLabelText } from './styled';
 
-const Labels = ({ albumTitle, artist }) => {
-  return (
-    <LabelsWrapper>
-      <Text>
-        {albumTitle}
-      </Text>
-      <BolderLabelText>
-        {artist}
-      </BolderLabelText>
-    </LabelsWrapper>
-  );
-};
+const Labels = ({ albumTitle, artist }) => (
+  <LabelsWrapper>
+    <Text>
+      {albumTitle}
+    </Text>
+    <BolderLabelText>
+      {artist}
+    </BolderLabelText>
+  </LabelsWrapper>
+);
 
 export default Labels;
