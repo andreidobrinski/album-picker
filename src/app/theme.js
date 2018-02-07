55b39158d1463156d2af390c36910ab16c780fd8
Andrei Dobrinski <>
2018-02-07 18:53:47 -0500

    cleanup

--- src/app/theme.js
+++ src/app/theme.js
@@ -3,20 +3,21 @@ import { css } from 'styled-components';
 const sizes = {
   desktop: 992,
   tablet: 768,
-}
+};
 
 // iterate through the sizes and create a media template
 export const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-  const emSize = sizes[label] / 16
+  const emSize = sizes[label] / 16;
+  // eslint-disable-next-line no-param-reassign
   accumulator[label] = (...args) => css`
     @media (min-width: ${emSize}em) {
       ${css(...args)}
     }
-  `
-  return accumulator
-}, {})
+  `;
+  return accumulator;
+}, {});
 
 export const colours = {
   main: '#7BB9FF',
@@ -27,7 +28,7 @@ export const colours = {
 
 export const fonts = {
   raleway: '"Raleway", sans-serif',
-}
+};
 
 export const spacer = '16px';
 
