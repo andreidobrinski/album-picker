be6e03895c19674ae3dee4ee6be3d3f39fc0bbaf
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-05-12 16:28:29 -0400

    add links to respective Google Sheet from curator label

--- src/app/labels/curator-label.js
+++ src/app/labels/curator-label.js
@@ -2,17 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper, Text } from './styled';
 
-const CuratorLabel = ({ curator }) => (
-  <Wrapper>
-    <Text>
-      {curator === 1 && 'Andrei'}
-      {curator === 2 && 'Amanda'}
-      {curator === 3 && 'Aaron'}
-      {curator === 4 && 'Kathleen and Kyle'}
-      &apos;s list
-    </Text>
-  </Wrapper>
-);
+const CuratorLabel = ({ curator }) => {
+  const getCuratorId = () => {
+    switch (curator) {
+      case 1:
+        return 0;
+      case 2:
+        return 1921160313;
+      case 3:
+        return 879339051;
+      case 4:
+        return 796619533;
+      default:
+        return 0;
+    }
+  };
+
+  const getCuratorList = () => {
+    const curatorId = getCuratorId();
+    return (
+      `https://docs.google.com/spreadsheets/d/1fRgVp73YruyRfUAjzigPXbC_TBsfiNcXJKHpFhLkPxs/edit#gid=${curatorId}`
+    );
+  };
+
+  return (
+    <Wrapper>
+      <a
+        href={getCuratorList()}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none' }}
+      >
+        <Text>
+          {curator === 1 && 'Andrei'}
+          {curator === 2 && 'Amanda'}
+          {curator === 3 && 'Aaron'}
+          {curator === 4 && 'Kathleen and Kyle'}
+          &apos;s list
+        </Text>
+      </a>
+    </Wrapper>
+  );
+};
 
 CuratorLabel.propTypes = {
   curator: PropTypes.number.isRequired,
